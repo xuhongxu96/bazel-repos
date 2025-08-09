f312ac2b2b7a9c11fe9c5c03abf68596a6479e71
Hongxu Xu <h445xu@uwaterloo.ca>
2025-08-09 20:53:55 +0000

    feat: add rebuild cost dataset

--- README.md
+++ README.md
@@ -4,11 +4,14 @@ Datasets for (**NOT ONLY**) Bazel-Based Projects (Repositories) in GitHub.
 
 ## Overview
 
-This project includes 3 datasets:
+This project includes 4 datasets:
 
 1. **search_repos/repos:** We have collected 126082 repositories using GitHub Search API. Filter criteria is as follows (searching details will be explained later):
    ```
    size:>1000 pushed:>2025-01-01 stars:<={end_star} fork:false archived:false template:false mirror:false
+
+   sort="stars"
+   order="desc"
    ```
 
 2. **search_repos/trees:** We have collected the file entries in root directory for 126049 repositories (126049 < 126082, as some repositories may have been removed).
@@ -20,7 +23,18 @@ This project includes 3 datasets:
    or path.endswith('.bazel')
    ```
 
+4. **get_rebuild_costs/results:** We successfully cloned 539 out of the 560
+filtered repositories and executed the Bazel query command to extract each
+project's dependency graph. Subsequently, we computed the rebuild costs using
+the `depstat` tool, available at
+[https://github.com/xuhongxu96/depreduce](https://github.com/xuhongxu96/depreduce)
+(currently private).
+
+   > Note: Some repositories could not be cloned, or their Bazel query results
+   > could not be parsed. These cases are excluded from the final dataset.
+
 ## Searching Strategy
+
 Due to the GitHub Search API's limitation of returning only the first 1000 results
 per query, we employ a slicing strategy based on repository star counts to
 maximize data coverage. Specifically, we partition the search space into
---
+++ get_rebuild_costs/get_rebuild_costs.py
@@ -0,0 +1,53 @@
+import os
+import glob
+import json
+import subprocess as sp
+from pprint import pprint
+
+DEPSTAT = "../../../depreduce/target/release/depstat"
+paths = sorted(glob.glob("../search_repos/filtered/*.json"))
+
+repos = []
+for path in paths:
+    with open(path, "r") as f:
+        data = json.loads(f.read())
+        if data:
+            repos.append((data['stargazers_count'], data['full_name'], data['clone_url']))
+
+repos = sorted(repos, key=lambda x: x[0], reverse=True)
+
+for repo in repos:
+    try:
+        stars, name, clone_url = repo
+        print(f"{stars} {name}")
+        repo_path = f"../cloned_repos/{name.replace('/', '__')}"
+
+        result_path = f"results/{name.replace('/', '__')}.json"
+        if os.path.exists(result_path):
+            print(f"Results for {name} already exist, skipping.")
+            continue
+
+        if os.path.exists(repo_path):
+            print(f"Repository {name} already cloned.")
+        else:
+            os.system(f"git clone {clone_url} {repo_path}")
+
+        p = sp.run([DEPSTAT, "-w", "."], cwd=repo_path, stdout=sp.PIPE, stderr=sp.PIPE)
+        stdout = p.stdout.decode("utf-8")
+        with open(f"results/{name.replace('/', '__')}.stdout", "w") as f:
+            f.write(stdout)
+        with open(f"results/{name.replace('/', '__')}.stderr", "w") as f:
+            f.write(p.stderr.decode("utf-8"))
+
+        rcost = (int(stdout.split("Rebuild cost: ")[1].strip()))
+        print(rcost)
+
+        with open(result_path, "w") as f:
+            json.dump({"repo": name, "rebuildCost": rcost}, f, indent=2)
+    except KeyboardInterrupt:
+        raise
+    except Exception as e:
+        print(f"Error processing {name}: {e}")
+        with open(f"results/{name.replace('/', '__')}.error", "w") as f:
+            f.write(str(e))
+            f.flush()
